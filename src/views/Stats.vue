34ff0cbe0e11818f9e207fdace1de47f103e5ad2
graysui <131964332+graysui@users.noreply.github.com>
2025-01-06 10:58:42 +0800

    Create Stats.vue

---
+++ src/views/Stats.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-card>
+      <div slot="header">
+        <span>监控统计信息</span>
+      </div>
+      <div>总文件数: {{ stats.total_files }}</div>
+      <div>文件变更数: {{ stats.files_changed }}</div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      stats: {
+        total_files: 0,
+        files_changed: 0,
+      },
+    };
+  },
+  created() {
+    this.fetchStats();
+  },
+  methods: {
+    async fetchStats() {
+      const response = await axios.get('/api/stats');
+      this.stats = response.data;
+    },
+  },
+};
+</script>
