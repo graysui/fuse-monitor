8ccdc6db5faa61ca07d09de22bf7fe3ed845855e
graysui <131964332+graysui@users.noreply.github.com>
2025-01-06 10:58:29 +0800

    Create Files.vue

---
+++ src/views/Files.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-table :data="files">
+      <el-table-column prop="path" label="文件路径"></el-table-column>
+      <el-table-column prop="status" label="状态"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      files: [],
+    };
+  },
+  created() {
+    this.fetchFiles();
+  },
+  methods: {
+    async fetchFiles() {
+      const response = await axios.get('/api/files');
+      this.files = response.data;
+    },
+  },
+};
+</script>
